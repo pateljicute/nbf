89d0cbe89887c1de451a48dc2838c8b49e9d5734
NBFHOMES <nbfhomes@gmail.com>
2025-12-01 09:41:08 +0530

    legha

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,7 @@ import { CartProvider } from '@/components/cart/cart-context';
 import { getCollections } from '@/lib/api';
 import { Header } from '../components/layout/header';
 import { AuthProvider } from '@/lib/auth-context';
+import { RealtimeProvider } from '@/lib/realtime-context';
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -31,13 +32,15 @@ export default async function RootLayout({
     <html lang="en">
       <body className={`${geistSans.variable} antialiased min-h-screen`}>
         <AuthProvider>
-          <CartProvider>
-            <NuqsAdapter>
-              <Header collections={collections} />
-              <Suspense>{children}</Suspense>
-              <Toaster closeButton position="bottom-right" />
-            </NuqsAdapter>
-          </CartProvider>
+          <RealtimeProvider>
+            <CartProvider>
+              <NuqsAdapter>
+                <Header collections={collections} />
+                <Suspense>{children}</Suspense>
+                <Toaster closeButton position="bottom-right" />
+              </NuqsAdapter>
+            </CartProvider>
+          </RealtimeProvider>
         </AuthProvider>
       </body>
     </html>
